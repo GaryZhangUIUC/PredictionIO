04906546960e4ceeb6d39e06cbf7ac7a5f250744
Gary Zhang <gary1412zhang@gmail.com>
2014-12-18 04:12:00 -0600

    TrainingData developed

---
+++ examples/experimental/scala-social-recommendation/src/main/scala/TrainingData.scala
@@ -0,0 +1,63 @@
+package io.prediction.recommendation.social
+
+import scala.collection.immutable.HashMap
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.DStream
+
+class TrainingData (
+  val userProfiles : RDD[(Int, Features)],
+  val userInteractions : RDD[((Int, Int), Interactions)],
+  val trainingRecord : DStream[TrainingRecord],
+  val sourceProfiles : RDD[(Int, Features)]
+) extends Serializable
+
+case class Features (
+  /*
+    categorcial features
+    similarity is 
+    the same or different
+    eg gender
+  */
+  val cfList : List[Int],
+  /*
+    numerical features
+    similarity is
+    based on the difference
+    eg age
+  */
+  val nfList : List[Double],
+  /*
+    list features
+    similarity is
+    based on the overlap
+    eg keywords
+  */
+  val lfList : List[List[HashMap[Int, Double]]]
+) extends Serializable
+
+case class Interactions (
+  /*
+    categorical interactions
+    where the category matters
+    eg following
+  */
+  val ciList : List[Int],
+  /*
+    numerical interactions
+    where the count matters
+    eg number of retweeting
+  */
+  val niList : List[Double]
+) extends Serializable
+
+case class TrainingRecord (
+  val fromId : Int,
+  val toId : Int,
+  val result : Boolean,
+  /*
+    extra information of a record
+    that can be used to weight the record
+  */
+  val ceList : List[Int],
+  val neList : List[Double]
+) extends Serializable
